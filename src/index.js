567f82bd65a8ada23f2866a47a8abaea14430bbd
ruxandra <pepicoal@yahoo.com>
2022-08-10 00:48:25 +0300

    remove folder button works correctly

--- src/index.js
+++ src/index.js
@@ -5,18 +5,45 @@ let folderList = [];
 class Folder {
     constructor(title) {
         this.title = title.value;
-        this.id = folderList.length;
         this.todos = [];
 
-        folderList.push(this);
+      //  folderList.push(this);
     }
 }
 
+(() => {
+
+/*function removeFolder(remove_folder_btn) {
+    let id = remove_folder_btn.id;
+    let folders_div = document.querySelectorAll(".folder");
+    for(let i = 0; i < folders_div.length; i++) {
+        if(i === id) {
+            left_panel.removeChild(folders_div[i]);
+        }
+        folderList.splice(i, 1);
+    }
+    
+}
+
+function redoID() {
+    let folders_div = document.querySelectorAll(".folder");
+    for(let i = 0; i < folders_div.length; i++) {
+
+        console.log(folders_div[i]);
+        console.log(folderList);
+
+        folders_div[i].id = i;
+        folderList[i].id = i;
+    }
+}*/
+
+const folders_div = document.getElementById("folders");
 const left_panel = document.getElementById("left_panel");
 function showFolder() {
+    left_panel.appendChild(folders_div);
     let folder_div = document.createElement("div");
     folder_div.classList.add("folder");
-    left_panel.appendChild(folder_div);
+    folders_div.appendChild(folder_div);
     let folder_btn = document.createElement("button");
     if(folderList[folderList.length-1].title.split("").length > 12) {
         folder_btn.innerText = folderList[folderList.length-1].title.slice(0, 12) + "...";
@@ -37,6 +64,23 @@ function showFolder() {
     remove_btn.classList.add("folder_remove");
     btn_div.appendChild(remove_btn);
     remove_btn.style.backgroundImage = "url('../src/img/remove.svg')";
+
+
+    let remove_folders_btn = document.querySelectorAll(".folder_remove");
+    remove_folders_btn.forEach(remove_folder_btn => {
+        remove_folder_btn.addEventListener("click", () => {
+            /*if(remove_folder_btn.id == folder_div.id) {
+                removeFolder(remove_folder_btn);
+                redoID();} */
+
+            //console.log(folderList.filter(e => e.title !== remove_folder_btn.parentElement.parentElement.innerText))
+            folderList = folderList.filter(e => e.title !== remove_folder_btn.parentElement.parentElement.innerText);
+            remove_folder_btn.parentElement.parentElement.remove();
+            
+        })
+        
+    })  
+    console.log(folderList);
 }
 
 const add_folder = document.getElementById("folder_submit");
@@ -44,10 +88,28 @@ let title = document.getElementById("folder_title");
 add_folder.addEventListener("click", (e) => {
     if(title.value != "") {
         e.preventDefault();
-        new Folder(title);
+        folderList.push(new Folder(title));
         title.value = "";
-        showFolder();
+        showFolder(); 
     }
 })
+
+})();
+
+/*function deleteFolder() {
+    
+}*/
+/*let remove_folders;
+const deleteFolder1 = (e) => {
+    e.parentElement.parentElement.remove();
+}
+remove_folders.forEach(remove_folder => {
+    console.log("b")
+    remove_folder.addEventListener("click", deleteFolder1);
+})*/
+
+
+
+
 
 //const add_task = document.getElementById("task_submit");

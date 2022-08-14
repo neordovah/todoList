f2bef01960a8420c1b5fbfe5b6c79fefc6987c93
ruxandra <pepicoal@yahoo.com>
2022-08-14 21:59:53 +0300

    add task works completely

--- src/index.js
+++ src/index.js
@@ -18,6 +18,14 @@ class Task {
 }
 
 (() => {
+const add_task_btn = document.getElementById("task_submit");
+
+let index;
+let clicked;
+
+if(folderList.length == 0) {
+    add_task_btn.hidden = true;
+}
 
 const folders_div = document.getElementById("folders");
 const left_panel = document.getElementById("left_panel");
@@ -27,15 +35,22 @@ if (localStorage.getItem('folders') === null) {
     folderList = [];
   }
 else {
+    add_task_btn.hidden = true;
     const foldersFromStorage = JSON.parse(localStorage.getItem('folders'));
     folderList = foldersFromStorage;
     for(let i = 0; i < folderList.length; i++) {
         showFolder(folderList[i]);
     }
+    const right_panel_h1 = document.getElementById("h1");
+    right_panel_h1.innerText = "To do list";
+    document.getElementById("tasks").innerHTML = "";
+    add_task_btn.hidden = true;
 }
 
 
 function showFolder(folder) {
+    clicked = 0;
+    add_task_btn.hidden = false;
     localStorage.clear();
     localStorage.setItem('folders', JSON.stringify(folderList));
     left_panel.appendChild(folders_div);
@@ -63,7 +78,28 @@ function showFolder(folder) {
     edit_input.value = folder.title;
     edit_input.readOnly = true;
 
+    let index3 = folderList.length-1;
+    const right_panel_h1 = document.getElementById("h1");
+    right_panel_h1.innerText = edit_input.value;
+    const tasks_div = document.getElementById("tasks");
+    tasks_div.innerHTML = "";
+    for(let i = 0; i < folderList[index3].todos.length; i++) {
+        let toggle = 0;
+        let toggle2 = 1;
+        let toggle3 = 0;
+        showTask(index3, i, toggle, toggle2, toggle3);
+    }
+
     remove_btn.addEventListener("click", (e) => {
+        const right_panel_h1 = document.getElementById("h1");
+        if(edit_input.value == right_panel_h1.innerText) {
+            right_panel_h1.innerText = "To do list";
+            document.getElementById("tasks").innerHTML = "";
+            add_task_btn.hidden = true;
+        }
+        if(folderList.length == 0) {
+            add_task_btn.hidden = true;
+        }
         let index = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
         folderList = [].concat(folderList.slice(0, index), folderList.slice(index+1, folderList.length));
         remove_btn.parentElement.parentElement.remove();
@@ -108,7 +144,9 @@ function showFolder(folder) {
 
     edit_input.addEventListener("click", (e) => {
         if(edit_input.readOnly == true) {
-            let index = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
+            clicked = 1;
+            add_task_btn.hidden = false;
+            index = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
             const right_panel_h1 = document.getElementById("h1");
             right_panel_h1.innerText = edit_input.value;
             const tasks_div = document.getElementById("tasks");
@@ -125,13 +163,18 @@ function showFolder(folder) {
     const folder_completed = document.getElementById("folder_completed");
     folder_completed.addEventListener("click", () => {
         const right_panel_h1 = document.getElementById("h1");
-            right_panel_h1.innerText = "Completed";
+        right_panel_h1.innerText = "Completed";
+        add_task_btn.hidden = true;
+        tasks_div.innerHTML = "";
+
     })
     
     const folder_notCompleted = document.getElementById("folder_notCompleted");
     folder_notCompleted.addEventListener("click", () => {
         const right_panel_h1 = document.getElementById("h1");
             right_panel_h1.innerText = "Past due";
+            add_task_btn.hidden = true;
+            tasks_div.innerHTML = "";
     })
 }
     
@@ -148,12 +191,10 @@ add_folder.addEventListener("click", (e) => {
 })
 
 function showTask(index, i, toggle, toggle2, toggle3) {
-    console.log(index)
-    console.log(i)
     const tasks_div = document.getElementById("tasks");
     const task_div = document.createElement("div");
     task_div.classList.add("task");
-    tasks_div.appendChild(task_div);
+    tasks_div.prepend(task_div)
     const task_checkbox = document.createElement("input");
     task_checkbox.type = "checkbox";
     task_div.appendChild(task_checkbox);
@@ -163,6 +204,7 @@ function showTask(index, i, toggle, toggle2, toggle3) {
     task_div.appendChild(task_input);
     const task_importance = document.createElement("select");
     task_importance.setAttribute("disabled", "true");
+    task_checkbox.removeAttribute("disabled");
     task_div.appendChild(task_importance);
     const importance_low = document.createElement("option");
     importance_low.value = "low";
@@ -197,7 +239,6 @@ function showTask(index, i, toggle, toggle2, toggle3) {
         task_input.value = folderList[index].todos[i].input;
         task_importance.value = folderList[index].todos[i].importance;
         task_date.value = folderList[index].todos[i].date;
-        console.log(folderList[index].todos[i].date)
         task_input.readOnly = true;
         task_importance.readOnly = true;
         task_date.readOnly = true;
@@ -209,6 +250,7 @@ function showTask(index, i, toggle, toggle2, toggle3) {
         body.style.pointerEvents = "none";
         task_div.style.pointerEvents = "auto";
         task_importance.removeAttribute("disabled");
+        task_checkbox.setAttribute("disabled", "true");
         task_input.focus();
     }
 
@@ -218,22 +260,28 @@ function showTask(index, i, toggle, toggle2, toggle3) {
             toggle = 1;
             task_input.readOnly = true;
             task_importance.setAttribute("disabled", "true");
+            task_checkbox.removeAttribute("disabled");
             task_date.readOnly = true;
             body.style.pointerEvents = "auto";
             edit_btn.style.backgroundImage = "url('../src/img/edit.svg')";
-            for(let i = 0; i < folderList.length; i++) {
-                if(document.getElementById("h1").innerHTML == folderList[i].title) {
+                   // index = index3;
                     if(toggle2 == 0) {
-                       folderList[i].todos.push(new Task(task_input, task_importance, task_date));
+                       // if(index !== undefined) {
+
+                            console.log(index)
+                            folderList[index].todos.push(new Task(task_input, task_importance, task_date));
+                      //  }
+                       // else {
+                           // console.log(folderList.length-1)
+                           // folderList[folderList.length-1].todos.push(new Task(task_input, task_importance, task_date));
+                       // }
                     }
                     else {
-                        let index = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
-                        folderList[i].todos[index].input = task_input.value;
-                        folderList[i].todos[index].importance = task_importance.value;
-                        folderList[i].todos[index].date = task_date.value;
+                        let index2 = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
+                        folderList[index].todos[i].input = task_input.value;
+                        folderList[index].todos[i].importance = task_importance.value;
+                        folderList[index].todos[i].date = task_date.value;
                     }
-                }
-            }
             localStorage.clear();
             localStorage.setItem('folders', JSON.stringify(folderList));
         }
@@ -241,34 +289,31 @@ function showTask(index, i, toggle, toggle2, toggle3) {
             toggle = 0;
             task_input.readOnly = false;
             task_importance.removeAttribute("disabled");
+            task_checkbox.setAttribute("disabled", "true");
             task_date.readOnly = false;
             edit_btn.style.backgroundImage = "url('../src/img/done.svg')";
-            body.style.pointerEvents = "none";
             task_div.style.pointerEvents = "auto";
+            body.style.pointerEvents = "none";
+            
         }
     })
 
     remove_btn.addEventListener("click", (e) => {
-        let index = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
-        for(let i = 0; i < folderList.length; i++) {
-            if(document.getElementById("h1").innerHTML == folderList[i].title) {
-                folderList[i].todos = [].concat(folderList[i].todos.slice(0, index), folderList[i].todos.slice(index+1, folderList[i].todos.length))
+        let index2 = Array.from(e.target.parentElement.parentElement.parentElement.children).indexOf(e.target.parentElement.parentElement);
+                folderList[index].todos = [].concat(folderList[index].todos.slice(0, index2), folderList[index].todos.slice(index2+1, folderList[index].todos.length))
                 remove_btn.parentElement.parentElement.remove();
                 body.style.pointerEvents = "auto";
                 localStorage.clear();
                 localStorage.setItem('folders', JSON.stringify(folderList));
-            }
-        }
     })
 }
 
-const add_task_btn = document.getElementById("task_submit");
-add_task_btn.addEventListener("click", () => {
-    const right_panel_h1 = document.getElementById("h1");
-    for(let i = 0; i < folderList.length; i++) {
-        if(right_panel_h1.innerText == folderList[i].title) {
-            showTask(0, 0, 0, 0, 1);
-        }
+add_task_btn.addEventListener("click", (e) => {
+    if(clicked == 1) {
+            showTask(index, 0, 0, 0, 1);
+    }
+    else {
+        showTask(folderList.length-1, 0, 0, 0, 1);
     }
 })
 
